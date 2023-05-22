99fd4e4e7cd0fed4c8d92b57669a63fc120be15c
SemiStruhar <74509099+tipyvsehodruhu@users.noreply.github.com>
2023-05-22 15:37:04 +0200

    Create ZaverecnyProjekt.java

---
+++ ZaverecnyProjekt.java
@@ -0,0 +1,15 @@
+package cz.itnetwork.zaverecnyprojekt;
+import java.util.*;
+
+public class ZaverecnyProjekt {
+
+    public static void main(String[] args) throws InterruptedException {
+ 
+        Program program = new Program();
+        
+        program.uvod();
+        program.vypisInterakce();
+        program.konecInterakce();
+        
+    }
+}
